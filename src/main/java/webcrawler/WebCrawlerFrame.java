b52c40b91fee2f1f57b539de35a2ad474269c6ce
ljurak <lucas.jurak@gmail.com>
2020-08-23 06:07:25 +0200

    Add logic for downloading webpage source code

--- src/main/java/webcrawler/WebCrawlerFrame.java
+++ src/main/java/webcrawler/WebCrawlerFrame.java
@@ -1,13 +1,23 @@
 package webcrawler;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import java.awt.BorderLayout;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class WebCrawlerFrame extends JFrame {
 
-    private static final int DEFAULT_WIDTH = 300;
+    private static final int DEFAULT_WIDTH = 500;
 
-    private static final int DEFAULT_HEIGHT = 300;
+    private static final int DEFAULT_HEIGHT = 400;
 
     private static final String DEFAULT_TITLE = "Web crawler";
 
@@ -16,10 +26,34 @@ public class WebCrawlerFrame extends JFrame {
         setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
 
         JTextArea content = new JTextArea();
-        content.setName("TextArea");
-        content.setText("HTML code?");
-        content.setEnabled(false);
+        content.setName("HtmlTextArea");
+        content.setEditable(false);
+        content.setLineWrap(true);
+        content.setWrapStyleWord(true);
 
-        add(content);
+        JTextField urlField = new JTextField(30);
+        urlField.setName("UrlTextField");
+
+        JButton downloadButton = new JButton("Get text!");
+        downloadButton.setName("RunButton");
+
+        downloadButton.addActionListener(event -> {
+            String url = urlField.getText();
+            try (InputStream in = new BufferedInputStream(new URL(url).openStream())) {
+                String page = new String(in.readAllBytes(), StandardCharsets.UTF_8);
+                content.setText(page);
+            } catch (IOException e) {
+                content.setText("");
+                content.append("Error occurred:\n");
+                content.append(e.getMessage());
+            }
+        });
+
+        JPanel controlPanel = new JPanel();
+        controlPanel.add(urlField);
+        controlPanel.add(downloadButton);
+
+        add(controlPanel, BorderLayout.NORTH);
+        add(new JScrollPane(content));
     }
 }
