68b2009803368745dc337c6418491eb55b2c50e3
ljurak <lucas.jurak@gmail.com>
2020-08-24 04:51:39 +0200

    Add extracting HTML page title

--- src/main/java/webcrawler/WebCrawlerFrame.java
+++ src/main/java/webcrawler/WebCrawlerFrame.java
@@ -2,28 +2,50 @@ package webcrawler;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import java.awt.BorderLayout;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WebCrawlerFrame extends JFrame {
 
     private static final int DEFAULT_WIDTH = 500;
 
-    private static final int DEFAULT_HEIGHT = 400;
+    private static final int DEFAULT_HEIGHT = 600;
 
-    private static final String DEFAULT_TITLE = "Web crawler";
+    private static final String DEFAULT_TITLE = "Web Crawler";
+
+    private static final Pattern titlePattern = Pattern.compile("<title>(.*?)</title>");
 
     public WebCrawlerFrame() {
         setTitle(DEFAULT_TITLE);
         setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+        setLayout(null);
+
+        JLabel urlLabel = new JLabel("URL:");
+        urlLabel.setBounds(10, 10, 50, 25);
+
+        JTextField urlField = new JTextField();
+        urlField.setName("UrlTextField");
+        urlField.setBounds(60, 10, 300, 25);
+
+        JButton parseButton = new JButton("Parse!");
+        parseButton.setName("RunButton");
+        parseButton.setBounds(370, 10, 100, 25);
+
+        JLabel titleLabel = new JLabel("Title:");
+        titleLabel.setBounds(10, 45, 50, 25);
+
+        JLabel titleContent = new JLabel("");
+        titleContent.setName("TitleLabel");
+        titleContent.setBounds(60, 45, 400, 25);
 
         JTextArea content = new JTextArea();
         content.setName("HtmlTextArea");
@@ -31,29 +53,33 @@ public class WebCrawlerFrame extends JFrame {
         content.setLineWrap(true);
         content.setWrapStyleWord(true);
 
-        JTextField urlField = new JTextField(30);
-        urlField.setName("UrlTextField");
+        JScrollPane scrollPane = new JScrollPane(content);
+        scrollPane.setBounds(10, 80, 460, 470);
 
-        JButton downloadButton = new JButton("Get text!");
-        downloadButton.setName("RunButton");
-
-        downloadButton.addActionListener(event -> {
+        parseButton.addActionListener(event -> {
             String url = urlField.getText();
             try (InputStream in = new BufferedInputStream(new URL(url).openStream())) {
                 String page = new String(in.readAllBytes(), StandardCharsets.UTF_8);
+                Matcher matcher = titlePattern.matcher(page);
+                if (matcher.find()) {
+                    String title = matcher.group(1);
+                    titleContent.setText(title);
+                }
                 content.setText(page);
             } catch (IOException e) {
+                titleContent.setText("");
+
                 content.setText("");
                 content.append("Error occurred:\n");
                 content.append(e.getMessage());
             }
         });
 
-        JPanel controlPanel = new JPanel();
-        controlPanel.add(urlField);
-        controlPanel.add(downloadButton);
-
-        add(controlPanel, BorderLayout.NORTH);
-        add(new JScrollPane(content));
+        add(urlLabel);
+        add(urlField);
+        add(parseButton);
+        add(titleLabel);
+        add(titleContent);
+        add(scrollPane);
     }
 }
